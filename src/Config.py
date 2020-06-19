f6c46a59ed37a83ae4d75ee41f2cf2f8d65d1919
Kåre Jørgensen <kaare1994@gmail.com>
2020-06-19 16:15:20 +0200

    Added rollover to logs

--- src/Config.py
+++ src/Config.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from logging.handlers import RotatingFileHandler
 
 import tensorflow as tf
 
@@ -17,10 +18,14 @@ class Config(object):
         logging_path = args.logging_path
         if not os.path.exists(logging_path):
             os.mkdir(logging_path)
+        file_path = "{}/main.log".format(logging_path)
+        rotating_file_handler = RotatingFileHandler(filename=file_path, backupCount=5)
+        if os.path.isfile(file_path):
+            rotating_file_handler.doRollover()
         logging.basicConfig(level=logging.getLevelName(args.logging),
                             format="%(asctime)s [%(levelname)s] %(message)s",
                             handlers=[
-                                logging.FileHandler("{}/main.log".format(logging_path)),
+                                rotating_file_handler,
                                 logging.StreamHandler()
                             ])
         logging.info("Arguments: {}".format(args))
