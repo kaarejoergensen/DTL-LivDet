6670985faa5b8ff25ae1dc0eb65891dbfa07c88f
Kåre Jørgensen <kaare1994@gmail.com>
2020-06-23 15:50:41 +0200

    Cache in memory

--- src/Dataset.py
+++ src/Dataset.py
@@ -86,7 +86,7 @@ class Dataset(object):
 
     def prepare_for_training(self, data_samples, data_sample_count, mode):
         dataset = tf.data.Dataset.from_tensor_slices(data_samples)
-        dataset = dataset.cache("{}/cache".format(self.config.args.data_path))
+        dataset = dataset.cache()
         dataset = dataset.shuffle(buffer_size=data_sample_count + 1)
         dataset = dataset.interleave(
             lambda x: tf.data.Dataset.from_tensors(x).map(self._process_path, num_parallel_calls=1),
